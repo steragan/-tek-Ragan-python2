08154fd4b7c836e033b6dc3f56a9fdd87da862b3
steragan <100454668+steragan@users.noreply.github.com>
2022-03-04 17:25:05 -0600

    Add files via upload

---
+++ dogs_database.sql
@@ -0,0 +1,31 @@
+use dogs1;
+
+select * from puppies;
+
+insert into puppies values
+(1,"Simba","brown"),
+(2,"Coco","black"),
+(3,"Ace","white"),
+(4,"Duke","brown"),
+(5,"Bear","brown"),
+(6,"Pepper","gray"),
+(7,"Rockey","black"),
+(8,"Stella","white"),
+(9,"Harley","black"),
+(10,"Daisy","brown");
+
+
+select * from owners;
+
+insert into owners values
+(1,"Steven",1),
+(2,"Sean",2),
+(11,"Eric",3),
+(4,"Adam",4),
+(5,"Bill",5),
+(6,"Luka",6),
+(7,"Reggie",7),
+(8,"Dirk",8),
+(9,"Amari",9),
+(10,"Angel",10);
+
